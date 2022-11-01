a20438be6dbf58d11eee23aa6da5735f52c4685e
Brierre <hlmbugle@gmail.com>
2022-10-31 20:31:05 -0400

    Week 9 project files added

--- src/main/resources/project_schema.sql
+++ src/main/resources/project_schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS project;
 
 CREATE TABLE project (
-project_id INT /*AUTO_INCREMENT*/ NOT NULL,
+project_id INT AUTO_INCREMENT NOT NULL,
 project_name VARCHAR(128) NOT null,
 estimated_hours DECIMAL(7,2),
 actual_hours DECIMAL(7,2),
@@ -15,7 +15,7 @@ PRIMARY KEY (project_id)
 );
 
 CREATE TABLE category (
-category_id INT /*AUTO_INCREMENT*/ NOT NULL,
+category_id INT AUTO_INCREMENT NOT NULL,
 category_name VARCHAR(128) NOT NULL,
 PRIMARY KEY (category_id)
 );
@@ -29,7 +29,7 @@ UNIQUE KEY (project_id, category_id)
 );
 
 CREATE TABLE step (
-step_id INT /*AUTO_INCREMENT*/ NOT NULL,
+step_id INT AUTO_INCREMENT NOT NULL,
 project_id INT NOT NULL,
 step_text TEXT NOT NULL,
 step_order TEXT NOT NULL,
@@ -38,7 +38,7 @@ FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
 
 CREATE TABLE material (
-material_id INT /*AUTO_INCREMENT*/ NOT NULL,
+material_id INT AUTO_INCREMENT NOT NULL,
 project_id INT NOT NULL,
 material_name VARCHAR(128) NOT NULL,
 num_required INT,
